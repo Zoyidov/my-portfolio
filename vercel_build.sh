6d530ecfe622adca14af37adfc7c1bbfc75de6bf
Zoyidov <nurmuxammadzoyidov@gmail.com>
2026-03-02 12:27:45 +0500

    vercel json added

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-# Flutter SDK yuklab olish
+# Flutter SDK
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter --version
 flutter config --enable-web
+
 flutter pub get
 flutter build web --release
